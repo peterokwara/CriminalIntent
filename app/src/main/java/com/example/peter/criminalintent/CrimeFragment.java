ccb19bcae8d4be7fb9dd58e48a9c7177468e9258
P.Okwara <ojakaapeter@gmail.com>
2017-02-22 23:25:04 +0300

    Complete the UI for fragment reporting

--- app/src/main/java/com/example/peter/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/example/peter/criminalintent/CrimeFragment.java
@@ -1,14 +1,15 @@
 package com.example.peter.criminalintent;
 
 import android.os.Bundle;
-import android.support.v4.app.BundleCompat;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.LayoutInflaterCompat;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 
 /**
@@ -17,18 +18,38 @@ import android.widget.EditText;
 public class CrimeFragment extends Fragment {
     private Crime mCrime;
     private EditText mTitleField;
+    private Button mDateButton;
+    private CheckBox mSolvedCheckBox;
 
     @Override
-    public void onCreate(Bundle savedInstanceState){
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mCrime = new Crime();
     }
 
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_crime, container, false);
 
-        /// Get a reference to the TitleField EditText widget and set up a listener for it
-        mTitleField = (EditText)v.findViewById(R.id.crime_title);
+        // Get a reference to the date button
+        // Show the date on the button
+        mDateButton = (Button) v.findViewById(R.id.crime_date);
+        mDateButton.setText(mCrime.getDate().toString());
+        mDateButton.setEnabled(false);
+
+        // Get a reference to the checkbox and add a listener to it
+        mSolvedCheckBox = (CheckBox)v.findViewById(R.id.crime_solved);
+        mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                // Set the crime's solved property
+                mCrime.setSolved(isChecked);
+
+            }
+        });
+
+        // Get a reference to the TitleField EditText widget and set up a listener for it
+        mTitleField = (EditText) v.findViewById(R.id.crime_title);
         mTitleField.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -45,7 +66,6 @@ public class CrimeFragment extends Fragment {
 
             }
         });
-
 
 
         return v;
