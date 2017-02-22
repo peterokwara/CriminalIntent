6ed56cd4b4100f8e0712cbee6f628e874770209a
P.Okwara <ojakaapeter@gmail.com>
2017-02-22 23:08:28 +0300

    Add new UI interface for crime reporting

--- app/src/main/java/com/example/peter/criminalintent/Crime.java
+++ app/src/main/java/com/example/peter/criminalintent/Crime.java
@@ -1,5 +1,6 @@
 package com.example.peter.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 import java.util.logging.StreamHandler;
 
@@ -7,6 +8,36 @@ import java.util.logging.StreamHandler;
  * Created by peter on 2/17/17.
  */
 public class Crime {
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public Crime() {
+
+        // Generate unique identifier
+        mId = UUID.randomUUID();
+
+        // Date
+        mDate = new Date();
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+
     public UUID getId() {
         return mId;
     }
@@ -15,21 +46,11 @@ public class Crime {
         mId = id;
     }
 
-    private UUID mId;
-
     public String getTitle() {
         return mTitle;
     }
 
     public void setTitle(String title) {
         mTitle = title;
-    }
-
-    private String mTitle;
-
-    public Crime(){
-
-        // Generate unique identifier
-        mId = UUID.randomUUID();
     }
 }
